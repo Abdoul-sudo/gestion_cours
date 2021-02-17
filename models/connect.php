a0342f09c2b879dba570752ff47c132e0efa7409
Abdoul <abdoul.ismael@esti.mg>
2021-02-17 20:03:05 +0300

    connection bdd sécurisée

--- models/connect.php
+++ models/connect.php
@@ -1,13 +1,19 @@
 <?php
     class DB_CONNECT{
-        public function dbConnect(){
+        private $DB_HOST = 'localhost';
+        private $DB_USER = 'root';
+        private $DB_PASS = '';
+        private $DB_NAME = 'bdd';
+
+        protected function dbConnect(){
             // Les configurations de la base de donnée
+            $DB_HOST = $this -> DB_HOST;
+            $DB_NAME = $this -> DB_NAME;
+            $DB_USER = $this -> DB_USER;
+            $DB_PASS = $this -> DB_PASS;
 
             if (!isset($DB_HOST)) {
-                $DB_HOST = 'localhost';
-                $DB_USER = 'root';
-                $DB_PASS = '';
-                $DB_NAME = 'bdd';
+                
             } 
             try{
                 $db = new PDO("mysql:host=" . $DB_HOST . ";dbname=" . $DB_NAME, $DB_USER, $DB_PASS);

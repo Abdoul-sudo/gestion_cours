1582ce596742c9b4774c378ff9b08de0ae7bb3ba
Abdoul <abdoul.ismael@esti.mg>
2021-02-18 16:43:51 +0300

    reste hash mdp pour résoudre bugs

--- models/getUser.php
+++ models/getUser.php
@@ -1,12 +1,11 @@
 <?php
-    require_once("connect.php");
+	require_once('../models/connect.php');
 
 	class M_USER extends DB_CONNECT 
 	{
 		public function getUser($status, $email)
 		{
-			require('../models/connect.php');
-			$db = dbConnect();
+			$db = $this -> dbConnect();
 			$sql = 'SELECT * FROM '. $status .' WHERE email_'. $status .' = ? LIMIT 1';
 			$req = $db->prepare($sql);
 			$req->execute(array($email));

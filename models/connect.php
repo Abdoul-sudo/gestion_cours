f77bae03d86a35b7104c65ec0d19623e7d8a0afe
fabienjulio <fabienjulio5@gmail.com>
2021-02-28 12:46:48 +0300

    suppression identifiants base de données

---
+++ models/connect.php
@@ -0,0 +1,23 @@
+<?php
+    class Database{
+        private $_dbHost = 'localhost';
+        private $_dbUser = 'fabien';
+        private $_dbPassword = 'Fabien512';
+        private $_dbName = 'gestion_cours';
+
+        protected function dbConnect(){
+            try{
+                $db = new PDO("mysql:host=" . $this->dbHost() . ";dbname=".$this->dbName(), $this->dbUser(), $this->dbPassword(), array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION));
+                return $db ;
+                
+            } catch(PDOException $error){
+                print "[ERREUR] Connexion échouée :" . $error->getMessage();
+                die();
+            }
+        }
+        //les accesseurs
+        public function dbHost(){ return $this->_dbHost; }
+        public function dbUser(){ return $this->_dbUser; }
+        public function dbPassword(){ return $this->_dbPassword; }
+        public function dbName(){ return $this->_dbName; }
+    }
